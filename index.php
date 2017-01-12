15720a831ed3fabd13ebadeb56d8c913794ad306
Quentin Penault <quentinpenault@live.fr>
2017-01-12 11:30:13 +0100

    add index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-FOOTBALL MANAGER
+<h1>FOOTBALL MANAGER</h1>
 
 <link rel="stylesheet" type="text/css" href="footballmanager.css">
 <img src="ballon.jpg">
+
+<?php 
+	require_once('database.php');
+	$instanceBDD = BDD::getInstance();
+	$requete = $instanceBDD->bdd->query("SELECT * FROM players");
+	$resultat = $requete->fetchAll();
+	return $resultat;
+?>
