cd02951971f0211ea3350e97c85b8c028283a0ff
Lucas <blastorwesker@gmail.com>
2017-01-12 12:04:42 +0100

    patch bdd

--- footballmanager.sql
+++ footballmanager.sql
@@ -17,6 +17,7 @@ CREATE TABLE Team(
         score_team       Int ,
         pointnumber_team Int NOT NULL ,
         coach_team       Varchar (25) NOT NULL ,
+        img_src_team     Varchar(64),
         PRIMARY KEY (id_team )
 )ENGINE=InnoDB;
 
@@ -33,6 +34,7 @@ CREATE TABLE Player(
         shirtnumber_player Varchar (25) NOT NULL ,
         score_player       Int NOT NULL ,
         position_player    Int NOT NULL ,
+        img_src_player     Varchar(64),
         id_team            Int NOT NULL ,
         PRIMARY KEY (id_player )
 )ENGINE=InnoDB;
@@ -64,3 +66,22 @@ CREATE TABLE participate(
 ALTER TABLE Player ADD CONSTRAINT FK_Player_id_team FOREIGN KEY (id_team) REFERENCES Team(id_team);
 ALTER TABLE participate ADD CONSTRAINT FK_participate_id_team FOREIGN KEY (id_team) REFERENCES Team(id_team);
 ALTER TABLE participate ADD CONSTRAINT FK_participate_id_matchs FOREIGN KEY (id_matchs) REFERENCES Matchs(id_matchs);
+
+INSERT INTO Team VALUES
+
+(1,"Real Madrid",610000000,null,0,"Zinédine Zidane"),
+(2,"FC Barcelone",585000000,null,0,"Luis Enrique"),
+(3,"Bayern de Munich",520000000,null,0,"Carlo Ancelotti"),
+(4,"Paris Saint Germain",500000000,null,0,"Unai Emery"),
+(5,"Manchester City",474000000,null,0,"Pep Guardiola"),
+(6,"Arsenal"455000000,null,0,"Arsène Wenger"),
+(7,"Chelsea",417000000,null,0,"Antonio Conte"),
+(8,"Liverpool",385000000,null,0,"Jürgen Klopp"),
+(9,"Juventus Turin",380000000,null,0,"Massimiliano Allegri"),
+(10,"Borussia Dortmund",352000000,null,0,"Thomas Tuchel")
+;
+
+INSERT INTO Player VALUES
+
+(1,"Keylor Navas",30,"male",1,85,)
+;
